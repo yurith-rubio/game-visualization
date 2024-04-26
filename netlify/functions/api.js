fe2a0f98fa3d1514c3572624dc314506b27ed61a
Yurith Rubio <iamyurith@gmail.com>
2024-04-26 16:50:56 +0200

    add steam api

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -5,22 +5,20 @@ import SteamAPI from "steamapi";
 const api = express();
 const steam = new SteamAPI(process.env.STEAM_API_KEY);
 
-app.get("/games", function (req, res) {
-  const id = req.params.gameid;
-  console.log("id inside get request");
-  console.log(id);
-  steam.getUserOwnedGames(process.env.STEAM_USER_ID).then(games => {
-    return res.json(games);
-  }).catch(error => {
-    console.error(error);
-    return res.send(500);
-  });
-});
-
-
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
-
+router.get("/games", function (req, res) {
+    const id = req.params.gameid;
+    console.log("id inside get request");
+    console.log(id);
+    steam.getUserOwnedGames(process.env.STEAM_USER_ID).then(games => {
+      return res.json(games);
+    }).catch(error => {
+      console.error(error);
+      return res.send(500);
+    });
+  });
+  
 api.use("/api/", router);
 
 export const handler = serverless(api);
